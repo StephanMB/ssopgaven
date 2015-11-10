bef3e5fb138f060718d5bf6af0c6aac9ae895bfb
StephanMB <stephan.braams@gmail.com>
2015-11-10 13:36:57 +0100

    created ThreeWayLampTest.java

---
+++ ssopgaven/src/ssOpgavenWeek1/ThreeWayLampTest.java
@@ -0,0 +1,54 @@
+package ssOpgavenWeek1;
+
+public class ThreeWayLampTest {
+
+	public ThreeWayLampTest(){
+		
+	}
+	
+	public void Test(ThreeWayLamp lamp){
+	
+		Boolean result1 = compare(lamp.getSetting(), "Off");
+		System.out.println("Test setting, should be Off: " + result1 );
+		
+		lamp.click();
+		Boolean result2 = compare(lamp.getSetting(), "Low");
+		System.out.println("Click! \n Should now be Low: " + result2 );
+		
+		lamp.click();
+		Boolean result3 = compare(lamp.getSetting(), "Medium");
+		System.out.println("Click! \n Should now be Medium: " + result3 );
+		
+		lamp.click();
+		Boolean result4 = compare(lamp.getSetting(), "High");
+		System.out.println("Click! \n Should now be High: " + result4 );
+		
+		lamp.click();
+		Boolean result5 = compare(lamp.getSetting(), "Off");
+		System.out.println(lamp.getSetting());
+		System.out.println("Click! \n Should now be Off: " + result5);
+		
+		if (result1 && result2 && result3 && result4 && result5) {
+			System.out.println("All tests succesfull!");
+		} else {
+			System.out.println("Test Failed");
+
+		}
+	}
+	
+	public Boolean compare(String real, String shouldBe){
+		if (real.equals(shouldBe)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ThreeWayLampTest test = new ThreeWayLampTest();
+		ThreeWayLamp lamp = new ThreeWayLamp();
+		test.Test(lamp);
+		
+	}
+
+}
